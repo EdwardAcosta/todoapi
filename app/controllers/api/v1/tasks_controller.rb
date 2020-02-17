63fe3bdf9796133d6482d5c25c26aa8ed7cd98b8
Edward Rene Acosta <edwardacosta@Edwards-Air.satx.rr.com>
2020-02-16 22:18:10 -0600

    second commit

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
 	
-		module Api::V1
-    		class TasksController < ApiController
-    		def index
-    		@tasks = Task.all
-    		render json: @tasks.to_json(only: [:name, :priority, :due_date])
-    		# render json: @tasks
-  			end
+	module Api::V1
+    	class TasksController < ApiController
+            def index
+                @tasks = Task.all
+    	        # render json: @tasks.to_json
+  			    render json: @tasks.to_json(only: [:name, :priority, :due_date])            
+    
+            end
     	end
 	end
 
---
+++ app/serializers/task_serializer.rb
@@ -0,0 +1,11 @@
+class TaskSerializer < ActiveModel::Serializer
+  # attributes :id
+  # attributes :id, :name, :priority, :due_date
+
+  	attributes :id, :name, :priority, :converted_due_date
+
+	  	def converted_due_date
+	  	object.due_date.strftime('%A, %b %d')
+  		end
+	end
+end
